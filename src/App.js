646b66f33aa8628ee5e038538b1e2afa05e0b9f6
Manuel Lopez <manuelfx117@gmail.com>
2020-04-17 16:12:45 -0500

    Aplicacion terminada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React,{useState} from 'react';
-
+import {evaluate} from 'mathjs';
 import Header from './Components/Header.jsx';
 import Form from './Components/Form.jsx';
 
 const App=()=> {
   const [resultado,setResultado] = useState(null);
-  let renRes = null
-  if(resultado)renRes = <> 
-      <h2>Resultado: </h2>
-      {resultado.toString()}
-  </>
+  const [valorX,setValorX] = useState(null);
+  let renRes,renNum = null
+  if(resultado){
+    
+    renRes = <> 
+              <h2>Resultado: </h2>
+              {resultado.toString()}
+            </>
+    if(valorX){
+      let scope={
+        x:valorX
+      }    
+      let evaluacion = evaluate(resultado.toString(),scope)
+      renNum=<>
+              <h2>Resultado Numerico</h2>
+              {evaluacion}
+            </>
+    }
+  }
   return (
     <div className="App">
       <Header/>
       <div className="container">
         <div className="row">
           <div className="col">
-            <Form setResultado={setResultado}/>   
+            <Form setResultado={setResultado} setValorX={setValorX}/>   
             {renRes}
+            {renNum}
           </div>
         </div>
       </div>
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,22 +1,29 @@
 import React,{useState} from  'react';
 import axios from 'axios';
-const Form = ({setResultado}) =>{
+const Form = ({setResultado,setValorX}) =>{
   const [expresion,setExpresion] = useState(null);
   const [operacion,setOperacion] = useState("simplify");
   const hacerConsulta = (e)=>{
     e.preventDefault();
     let url = `https://newton.now.sh/${operacion}/${expresion}`;
-    axios.get(url).then(res=>{setResultado(res.data.result);console.log(res.data)});
+    axios.get(url).then(res=>{setResultado(res.data.result);});
 
   }
+  let inputX=null;
+  if(operacion==='derive'||operacion==='integrate'){
+    inputX = <div className="form-group col-2 d-flex align-items-center">
+                <input className="form-control" placeholder="valor de x" onChange={e=>{setValorX(e.currentTarget.value)}}/>
+              </div>
+  }else inputX = null;
+
   return(
     <form onSubmit={hacerConsulta}>
       <div className="row">
-        <div className="form-group col-5">
+        <div className="form-group col-4">
           <label>Expresión a usar: </label>
           <input className="form-control " type="text" placeholder="5(2x+5/x)" onChange={e=>{setExpresion(e.currentTarget.value);}}/>
         </div>
-        <div className="form-group col-5">
+        <div className="form-group col-4">
           <label>Operacion Matematica: </label>
           <select className="form-control" placeholder="elija una opcion" onChange={e=>{setOperacion(e.currentTarget.value);}}>
             <option value="simplify">Simplificar "2^2+2(2)"</option>
@@ -37,6 +44,7 @@ const Form = ({setResultado}) =>{
           </select>
           <small className="form-text text-muted"> **En cada opcion hay un ejemplo de input</small>
         </div>
+        {inputX}
         <div className="form-group col-2 d-flex align-items-center">
           <button className="btn btn-primary btn-block">Calcular</button>
         </div>
